2868f7381a7e533447bd2e244c2cfc32ea4d0c35
PatricioMedina <patriciomm8@hotmail.com>
2016-08-21 14:49:29 -0500

    Add files via upload

---
+++ mym.com.co/src/main/java/MyMcontodapropiedad/mym/com/co/App.java
@@ -0,0 +1,13 @@
+package MyMcontodapropiedad.mym.com.co;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
